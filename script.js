284bdf2a66fa91528d920792c44893599ee07018
Wojciech Grodzicki <wojciech_grodzicki@outlook.com>
2023-02-10 21:09:57 +0100

    JS_game_p3_movement

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const canvas = document.getElementById("canvas1");
 const context = canvas.getContext("2d");
 CANVAS_WIDTH = canvas.width = 500;
 CANVAS_HEIGHT = canvas.height = 1000;
-const numberEnemies = 100; // How many enemies we want
+const numberEnemies = 40; // How many enemies we want
 const enemies = []; // Empty array to store enemies
 let gameFrame = 0; // Variable to keep track of frames
 
@@ -11,27 +11,44 @@ class Enemy { // New class to create enemy objects
     constructor() {
         // Get the enemy sprite sheet
         this.image = new Image();
-        this.image.src = "resources/enemy2.png";
-        this.speed = Math.random() * 4 + 1;
-        // Randomize enemy position
-        this.x = Math.random() * canvas.width; // Math.random() generates a number between 0 and 1
-        this.y = Math.random() * canvas.height;
+        this.image.src = "resources/enemy4.png";
         // Randomize enemy speed
-        this.speed = Math.random() * 4 - 2; // This line of code generates a number between -2 and +2
+        this.speed = Math.random() * 4 + 1;
         // Size of a single image on the sprite sheet
-        this.spriteWidth = 266;
-        this.spriteHeigth = 188;
+        this.spriteWidth = 213;
+        this.spriteHeigth = 213;
         // Determine enemy size
         this.width = this.spriteWidth / 2.5;
         this.height = this.spriteHeigth / 2.5;
+        // Initial randomized enemy position
+        this.x = Math.random() * (canvas.width - this.width); // Math.random() generates a number between 0 and 1
+        this.y = Math.random() * (canvas.height - this.height); // Make sure enemies fit on the canvas
+        // New randomized enemy position
+        this.newX = Math.random() * (canvas.width - this.width);
+        this.newY = Math.random() * (canvas.height - this.height);
         this.frame = 0; // Keep track of frames
-        // Randomize wing flapping (animation speed)
+        // Randomize animation speed
         this.flapSpeed = Math.floor(Math.random() * 3 + 1); // Get a random number from 1 to 4 and make sure it's an int
+        this.interval = Math.floor(Math.random() * 200 + 50); // Randomize the interval between position changes
     }
     update() { // A custom class method to control enemy movement
-        this.x += this.speed;
-        this.y += this.speed;
-        // Run the animation every other frame to slow it down x2
+        // Every time gameFrame is divisible by 0 (every xth animation loop) assign new random values to positions
+        // The divisor determines the interval between position changes
+        if (gameFrame % this.interval == 0) {
+            this.newX = Math.random() * (canvas.width - this.width);
+            this.newY = Math.random() * (canvas.height - this.height);
+        }
+        // Calculate distance between positions
+        let dx = this.x - this.newX;
+        let dy = this.y - this.newY;
+        // Change position by a fraction of the distance
+        this.x -= dx / 50;
+        this.y -= dy /  50;
+        // The following check creates an endless flow of enemies right -> left
+        if (this.x + this.width < 0) {
+            this.x = canvas.width;
+        }
+        // Run the animation depending on flapSpeed
         if (gameFrame % this.flapSpeed == 0) { // Works fine when flapSpeed is an int (modulus doesn't work with floats as divisors!)
             // Ternary operator equivalent to:
             // If value > 4, then set value to 0
